186182d0ffc2bc23e1171161ffb381a9cf7df2b2
Yeison <enmanuelquiterios@gmail.com>
2021-11-25 23:01:18 -0500

    Added validation to the models

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,2 +1,7 @@
 class Business < ApplicationRecord
+    has_many :reviews 
+    has_many :users, through: :reviews
+
+    validates :name, presence: true, uniqueness: true
+    validates :category, :city, :state, :zip_code, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :reviews 
+    has_many :businesses, through: :reviews
+
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
