af5901605b85d54019246b519203ccd7f1027119
Yeison <enmanuelquiterios@gmail.com>
2021-11-25 21:09:20 -0500

    Created migrated and seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bob = User.create(username: "bobiscool", email: "bobiscool@123.com")
+sam = User.create(username: "samiam", email: "samiam@123.com")
+
+starbucks = Business.create(name: "Starbucks", category: "cafe", city: "north pole", state: "california", zip_code: 100099)
+mcdonalds = Business.create(name: "McDonalds", category: "fast-food", city: "south pole", state: "california", zip_code: 100099)
+dennys = Business.create(name: "Dennys", category: "diner", city: "los angeles", state: "california", zip_code: 100099)
+
+Review.create(user_id: bob.id, business_id: starbucks.id, content: 'Hello the best website ever Thanks')
